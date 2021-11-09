e9c6f415d8e7ba026c1eac56dd046226aaaa936c
masonjar <mason.whitaker79@gmail.com>
2021-11-09 07:59:17 -0800

    lalalala 4 done

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -90,27 +90,30 @@ const speaker = (words, callback) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
 
-Write a function named addValues that takes in an array and a value and pushes the value into the array. This function does not 
+Write a function named addValues that takes in an 
+array and a value and pushes the value into the array. This function does not 
 need a return statement.
 
 Then, write a function named addNumbers that takes in four arguments:
   - A number to be added to an array
   - An array into which the number should be added
   - The number of times the number should be added
-  - A callback function to use to add the numbers to the array (Hint: you already defined it)
+  - A callback function to use to add the numbers 
+  to the array (Hint: you already defined it)
 
-Within the addNumbers function, invoke the callback function as many times as necessary, based on the third argument of the 
-addNumbers function.
+Within the addNumbers function, invoke the callback 
+function as many times as necessary, based on the 
+third argument of the addNumbers function.
 
 Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
 const addValues = (arr, value) => {
-  // Solution code here...
+  return arr.push(value);
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  // Solution code here...
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
